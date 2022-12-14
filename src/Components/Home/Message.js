076b9ffaa0472995527e02f778447a4dff297f17
Arpit Kaithwar <arpitkaithwar02@gmail.com>
2022-12-14 23:52:14 +0530

    Footer Updated

---
+++ src/Components/Home/Message.js
@@ -0,0 +1,32 @@
+export default function Message() {
+  return (
+    <div className="container" id="message">
+      <div className="row my-2">
+        <h1>Recent Messages</h1>
+      </div>
+      <div className="row mes">
+        <div className="col">
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </p>
+        </div>
+      </div>
+      <div className="row my-5 getm">
+        <div className="col-9">
+            <h5>Get in touch with any questions, ideas, or feedback you may have.</h5>
+        </div>
+        <dv className="col-3">
+            <button type="button" class="mybtn">Contact</button>
+        </dv>
+      </div>
+    </div>
+  );
+}
