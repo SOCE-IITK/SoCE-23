76e146a575905e16851ab432065fa545dc85a3b8
Saurabh Sahay <saurabhsahay@Saurabhs-MacBook-Air.local>
2022-12-14 22:18:34 +0530

    added files

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,15 @@
 import './Styles/home.css'
 import Slider from './Home/Slider'
 import About from './Home/About'
+// import Hello from './Hello'
+import Message from './Home/Message'
 export default function Home(){
     return(
         <div id='home'>
+           
             <About/>
             <Slider/>
+            <Message/>
         </div>
     )
 }
