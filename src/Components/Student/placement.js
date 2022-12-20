fb484e94f01df9afef3023f76170e86b1a34c2f0
Saurabh Sahay <saurabhsahay@Saurabhs-MacBook-Air.local>
2022-12-20 19:46:25 +0530

    refactor student-section

--- src/Components/Student/placement.js
+++ src/Components/Student/placement.js
@@ -1,37 +1,41 @@
 export default function Placement() {
   return (
-    <div className="container Container" id="placement">
-    <div className="row Divs">
-      <div className="col-sm-5 stud">
-        <h1>Placement postulates</h1>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text
-          ever since the 1500s, when an unknown printer took a galley of type
-          and scrambled it to make a
-        </p>
+    <div className="container">
+      <div className="container Container" id="placement">
         <div className="row">
-          <div className="col">
-          <button type="button" className="btn Btn">Read more</button>
+          <div className="col-sm-1"></div>
+          <div className="col-sm-4 stud">
+            <h1>Placement postulates</h1>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of type
+              and scrambled it to make a
+            </p>
+            <div className="row">
+              <div className="col">
+                <button type="button" className="btn Btn">Read more</button>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      <div className="col-sm-2"></div>
-      <div className="col-sm-5 stud">
-        <h1>Career Choices</h1>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text
-          ever since the 1500s, when an unknown printer took a galley of type
-          and scrambled it to make a
-        </p>
-        <div className="row">
-          <div className="col">
-          <button type="button" className="btn Btn">Read more</button>
+          <div className="col-sm-2"></div>
+          <div className="col-sm-4 stud">
+            <h1>Career Choices</h1>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of type
+              and scrambled it to make a
+            </p>
+            <div className="row">
+              <div className="col">
+                <button type="button" className="btn Btn">Read more</button>
+              </div>
+            </div>
           </div>
+          <div className="col-sm-1"></div>
         </div>
       </div>
     </div>
-  </div>
   );
 }
--- src/Components/Student/session.js
+++ src/Components/Student/session.js
@@ -1,37 +1,41 @@
 export default function Session() {
   return (
-    <div className="container Container" id="session">
-    <div className="row">
-      <div className="col-sm-5 stud">
-        <h1>Sessions</h1>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text
-          ever since the 1500s, when an unknown printer took a galley of type
-          and scrambled it to make a
-        </p>
+    <div className="container">
+      <div className="container Container" id="session">
         <div className="row">
-          <div className="col">
-          <button type="button" className="btn Btn">Read more</button>
+          <div className="col-sm-1"></div>
+          <div className="col-sm-4 stud">
+            <h1>Sessions</h1>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of type
+              and scrambled it to make a
+            </p>
+            <div className="row">
+              <div className="col">
+                <button type="button" className="btn Btn">Read more</button>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      <div className="col-sm-2"></div>
-      <div className="col-sm-5 stud">
-        <h1>Merchandises</h1>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text
-          ever since the 1500s, when an unknown printer took a galley of type
-          and scrambled it to make a
-        </p>
-        <div className="row">
-          <div className="col">
-          <button type="button" className="btn Btn">Read more</button>
+          <div className="col-sm-2"></div>
+          <div className="col-sm-4 stud">
+            <h1>Merchandises</h1>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of type
+              and scrambled it to make a
+            </p>
+            <div className="row">
+              <div className="col">
+                <button type="button" className="btn Btn">Read more</button>
+              </div>
+            </div>
           </div>
+          <div className="col-sm-1"></div>
         </div>
       </div>
     </div>
-  </div>
   );
 }
