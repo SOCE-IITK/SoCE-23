d22cb6d46ec5589cf33913c46d627a9af5a7e587
Aashruti-21 <109677503+Aashruti-21@users.noreply.github.com>
2022-12-24 20:43:20 +0530

    gjj

---
+++ src/Components/placement/postulate.js
@@ -0,0 +1,27 @@
+export default function postulate() {
+    return (
+      <div className="container" id="message">
+        <div className="row my-2">
+          <h1>Placement Postulates</h1>
+        </div>
+        <div className="row mes">
+          <div className="col">
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of type
+              and scrambled it to make a type specimen book. It has survived not
+              only five centuries, but also the leap into electronic typesetting,
+              remaining .
+            </p>
+            <dv className="col-3">
+              <button type="button" class="mybtn">Y19</button>
+              <button type="button" class="mybtn">Y18</button>
+              <button type="button" class="mybtn">Y17</button>
+              <button type="button" class="mybtn">Y16</button>
+          </dv>
+          </div>
+        </div>
+      </div>
+    );
+  }
