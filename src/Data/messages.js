72a47c87d5c8a59e28d9a7a4b4dbc0e35161f885
Saurabh Sahay <saurabhsahay@Saurabhs-MacBook-Air.local>
2023-03-11 17:56:51 +0530

    update msg slider

--- src/Data/messages.js
+++ src/Data/messages.js
@@ -1,9 +1,9 @@
 const content = [
   [
-    "Please provide your final SPO placement and Intern(Optional) resumes for the resume database. This will be helpful for the junior batches to prepare a good resume of thier preferable profile.",
+    "Please register for the OpenLCA workshop dated on 15th March 2023 by Mr. Mohd. Kamil Vakil. Note that attendance alone does not guarantee your ratification unless you didn't register",
     "Open for all",
-    "March 10, 2023",
-    "https://forms.gle/FS7ycBFsQtGkcEUD6",
+    "Deadline: 14th March 2023",
+    "https://forms.gle/jT7yfxkT1Lx3iQFP9",
   ],
   [
     "Please provide your final SPO placement and Intern(Optional) resumes for the resume database. This will be helpful for the junior batches to prepare a good resume of thier preferable profile.",
