c36c114ee28bbb7629cdcf6848d8e95f25351e5a
Saurabh Sahay <saurabhsahay@Saurabhs-MacBook-Air.local>
2023-04-04 19:28:11 +0530

    added web team

--- src/database/web.js
+++ src/database/web.js
@@ -7,7 +7,7 @@ var web = [
             post: "Web Developer",
             fb: "https://www.facebook.com/amanks20",
             insta: "https://www.instagram.com/protonaman",
-            linkedin: "https://www.linkedin.com/in/amanks20/",
+            linkedin: "/team21/w4.jpg",
             mail: "mailto:amanks20@iitk.ac.in",
             image: "/img/team/d2.jpg"
         },
@@ -17,7 +17,7 @@ var web = [
             post: "Web Designer",
             fb: "https://www.facebook.com/amanks20",
             insta: "https://www.instagram.com/protonaman",
-            linkedin: "https://www.linkedin.com/in/amanks20/",
+            linkedin: "/cordi4.jpeg",
             mail: "mailto:amanks20@iitk.ac.in",
             image: "/img/team/d2.jpg"
         }
