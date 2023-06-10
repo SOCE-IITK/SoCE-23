77bd64710b491a1a29a7e2859e30c1d36d7dc457
VEDANT ADLAK <Vedantadlak2105@gmail.com>
2023-06-10 17:10:34 +0530

    minorbug

--- src/Components/Home/Slider.js
+++ src/Components/Home/Slider.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import $ from "jquery";
 import { content, img } from "../../Data/slider_data";
 
@@ -7,54 +7,54 @@ export default function Slider() {
   const [activeIndex, setActiveIndex] = useState(0);
 
   useEffect(() => {
-    universal(activeIndex); 
+    universal(activeIndex);
   }, []);
 
-function universal(i) {
-  setActiveIndex(i);
-  $("#y").animate({ opacity: 0 }, 400, function () {
-    $(this).html(content[i][0]).animate({ opacity: 1 }, 200);
-  });
-  $("#slider-heading").animate({ opacity: 0 }, 400, function () {
-    $(this).html(content[i][1]).animate({ opacity: 1 }, 200);
-  });
-  $("#slider-date").animate({ opacity: 0 }, 400, function () {
-    $(this).html(content[i][2]).animate({ opacity: 1 }, 200);
-  });
-  // document.getElementById('img').src=img[j];
-  $("#img").animate({ opacity: 0 }, 400, function () {
-    $(this).attr("src", img[i]).animate({ opacity: 1 }, 300);
-  });
-  // ********** see later for a tag property
-  $("#slider-link").attr("href", content[i][3]);
-  // console.log('#slider-l')
-  let h = document.getElementsByClassName("indicators");
-  for (var k = 0; k < 3; k++) {
-    h[k].classList.remove("fa-circle");
-    h[k].classList.add("fa-circle-thin");
+  function universal(i) {
+    setActiveIndex(i);
+    $("#y").animate({ opacity: 0 }, 400, function () {
+      $(this).html(content[i][0]).animate({ opacity: 1 }, 200);
+    });
+    $("#slider-heading").animate({ opacity: 0 }, 400, function () {
+      $(this).html(content[i][1]).animate({ opacity: 1 }, 200);
+    });
+    $("#slider-date").animate({ opacity: 0 }, 400, function () {
+      $(this).html(content[i][2]).animate({ opacity: 1 }, 200);
+    });
+    // document.getElementById('img').src=img[j];
+    $("#img").animate({ opacity: 0 }, 400, function () {
+      $(this).attr("src", img[i]).animate({ opacity: 1 }, 300);
+    });
+    // ********** see later for a tag property
+    $("#slider-link").attr("href", content[i][3]);
+    // console.log('#slider-l')
+    let h = document.getElementsByClassName("indicators");
+    for (var k = 0; k < 3; k++) {
+      h[k].classList.remove("fa-circle");
+      h[k].classList.add("fa-circle-thin");
+    }
+    h[i].classList.remove("fa-circle-thin");
+    h[i].classList.add("fa-circle");
   }
-  h[i].classList.remove("fa-circle-thin");
-  h[i].classList.add("fa-circle");
-}
-function customf() {
-  let nextIndex = activeIndex + 1;
-  if (nextIndex > 2) nextIndex = 0;
-  universal(nextIndex);
-}
-function customp() {
-  let prevIndex = activeIndex - 1;
-  if (prevIndex < 0) prevIndex = 2;
-  universal(prevIndex);
-}
-function i1() {
-  universal(0);
-}
-function i2() {
-  universal(1);
-}
-function i3() {
-  universal(2);
-}
+  function customf() {
+    let nextIndex = activeIndex + 1;
+    if (nextIndex > 2) nextIndex = 0;
+    universal(nextIndex);
+  }
+  function customp() {
+    let prevIndex = activeIndex - 1;
+    if (prevIndex < 0) prevIndex = 2;
+    universal(prevIndex);
+  }
+  function i1() {
+    universal(0);
+  }
+  function i2() {
+    universal(1);
+  }
+  function i3() {
+    universal(2);
+  }
 
   return (
     <div>
@@ -103,26 +103,29 @@ function i3() {
                               marginBottom: "10px",
                             }}
                           >
-                          <div id="y" style={{ maxWidth: "100%" }}>{content[0][0]} </div>{" "}
-                        </div>
+                            <div id="y" style={{ maxWidth: "100%" }}>{content[0][0]} </div>{" "}
+                          </div>
                         </div>
                         <div className="row readmore">
-                          <div className="col">
+                          <div className="col" style={{
+                            display: "flex",
+                            justifyContent: "flex-end",
+                          }}>
                             <div
                               style={{
                                 overflow: "hidden",
                               }}
                             >
-                            <a id="slider-link" href="" target={"_blank"}>
-                              <button
-                                id="read-more"
-                                className="btn btn-primary"
-                                style={{ marginBottom: "10px", maxWidth: "100%", }}
-                              >
-                                Read More
-                              </button>
-                            </a>
-                          </div>
+                              <a id="slider-link" href="" target={"_blank"}>
+                                <button
+                                  id="read-more"
+                                  className="btn btn-primary"
+                                  style={{ marginBottom: "10px", maxWidth: "100%", }}
+                                >
+                                  Read More
+                                </button>
+                              </a>
+                            </div>
 
                           </div>
                         </div>
@@ -175,4 +178,3 @@ function i3() {
     </div>
   );
 }
-
