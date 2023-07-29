8e1cb7f7111b9fd16cdadd951570d42f83402d50
VEDANT ADLAK <Vedantadlak2105@gmail.com>
2023-07-29 12:02:32 +0530

    y22 3rd sem informal session side added

--- src/Components/Archive/archive.js
+++ src/Components/Archive/archive.js
@@ -8,43 +8,54 @@ import ArBlogTemplate from './ArBlogTemplate';
 
 export default function Archive() {
     return (
-        <div id='StudentSec'>
-            
-            <ArchiveInfo/>
-            <ArOfflineSessionTemplate props={[
-                {
-                    head: "Y22 Informal Session",
-                    date: "10 June 2023",
-                    slides: "https://drive.google.com/drive/folders/1i0hH7tnFbqRiPIbWkSMvn43pBub-Eekw",
-                    
-                    
-                },
+      <div id="StudentSec">
+        <ArchiveInfo />
+        <ArOfflineSessionTemplate
+          props={[
+            {
+              head: "Y22 3rd Sem Informal Session",
+              date: "28 July 2023",
+              slides:
+                "https://drive.google.com/drive/folders/1C94XBvQAlmp1vliu9lpfDGOz4_XI7gDE",
+            },
+          ]}
+        />
+        <ArOfflineSessionTemplate
+          props={[
+            {
+              head: "Y22 Informal Session",
+              date: "10 June 2023",
+              slides:
+                "https://drive.google.com/drive/folders/1i0hH7tnFbqRiPIbWkSMvn43pBub-Eekw",
+            },
+          ]}
+        />
+        <ArOfflineSessionTemplate
+          props={[
+            {
+              head: "Informal Quant and ML Session",
+              date: "3 June 2023",
+              slides:
+                "https://drive.google.com/drive/folders/1iCdKGqqZTWGUWKP6qdinHYtwyOG3CPsL",
+            },
+          ]}
+        />
 
-            ]} />
-            <ArOfflineSessionTemplate props={[
-                {
-                    head: "Informal Quant and ML Session",
-                    date: "3 June 2023",
-                    slides: "https://drive.google.com/drive/folders/1iCdKGqqZTWGUWKP6qdinHYtwyOG3CPsL",
+        <ArBlogTemplate
+          props={[
+            {
+              head: "Blog: New Parliament Building",
+              date: "1 June 2023",
+              slides:
+                "https://www.linkedin.com/feed/update/urn:li:activity:7070107898684555264",
 
-
-                },
-
-            ]} />
-
-            <ArBlogTemplate props={[
-                {
-                    head: "Blog: New Parliament Building",
-                    date: "1 June 2023",
-                    slides: "https://www.linkedin.com/feed/update/urn:li:activity:7070107898684555264",
-                    
-                    insta: "https://www.instagram.com/p/Cs8NlxsL2v2/?igshid=MzRlODBiNWFlZA==",
-                    linkedin: "https://www.linkedin.com/feed/update/urn:li:activity:7070107898684555264",
-                },
-
-            ]} />
-
-            
-        </div>
-    )
+              insta:
+                "https://www.instagram.com/p/Cs8NlxsL2v2/?igshid=MzRlODBiNWFlZA==",
+              linkedin:
+                "https://www.linkedin.com/feed/update/urn:li:activity:7070107898684555264",
+            },
+          ]}
+        />
+      </div>
+    );
 }
