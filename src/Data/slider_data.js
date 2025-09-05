93ce11a06b63823490870aea362eb8fbf9e9c2ab
nikhilprajapati-21 <nikhilkumarprajapati2004@gmail.com>
2025-09-05 13:17:24 +0530

    test

--- src/Data/slider_data.js
+++ src/Data/slider_data.js
@@ -22,7 +22,7 @@ const content = [
   [
     "We have more activities in the pipeline that will be announced shortly. Keep checking this space for further information.",
     "Coming Soon",
-    "...",
+    "....",
     "https://www.instagram.com/society_of_civil_engineers/?hl=en",  
     "#"
   ],
