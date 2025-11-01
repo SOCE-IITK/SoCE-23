4762a86132640bf61565bf5ecea40bc115ab3920
nikhilprajapati-21 <nikhilkumarprajapati2004@gmail.com>
2025-11-01 19:42:08 +0530

    Save before merge

--- src/Components/Home/Message.js
+++ src/Components/Home/Message.js
@@ -1,8 +1,9 @@
-import { content } from '../../Data/messages'
+import { content } from "../../Data/messages";
 import $ from "jquery";
-var a = 0;
-function universal(i) {
 
+var a = 0;
+
+function universal(i) {
   $("#content").animate({ opacity: 0 }, 400, function () {
     $(this).html(content[i][0]).animate({ opacity: 1 }, 200);
   });
@@ -12,7 +13,7 @@ function universal(i) {
   $("#msg-date").animate({ opacity: 0 }, 400, function () {
     $(this).html(content[i][2]).animate({ opacity: 1 }, 200);
   });
-  // ********** see later for a tag property
+
   $("#msg-link").attr("href", content[i][3]);
 
   let h = document.getElementsByClassName("Indicators");
@@ -23,16 +24,19 @@ function universal(i) {
   h[i].classList.remove("fa-circle-thin");
   h[i].classList.add("fa-circle");
 }
+
 function customf() {
   a++;
   if (a > 1) a = 0;
   universal(a);
 }
+
 function customp() {
   a--;
   if (a < 0) a = 1;
   universal(a);
 }
+
 function i1() {
   universal(0);
 }
@@ -46,49 +50,34 @@ function i3() {
 export default function Message() {
   return (
     <div className="container">
-      <div className="container" id="me
-        <div className="mess">
-          {/* <h1>Recent message</h1> */}
-        </div>
-
-        {/* <div className="mess">
-          <h1>Recent message</h1>
-        </div> */}
+      <div className="container" id="message">
+        <div className="mess">{/* <h1>Recent message</h1> */}</div>
 
         <div className="row mes">
           <div className="col">
-            {/* <h6>
-              <i id='open'>{content[0][1]}</i>
-            </h6> */}
+            <p id="content">{content[0][0]}</p>
+            <b id="msg-date">{content[0][2]}</b>
 
-            <p id='content'>
-
-            {/* <p id='content'>
-
-              {content[0][0]}
-              
-            </p> */}
-            <b id='msg-date'>{content[0][2]}</b>
-           <div className="col-sm-8.2">
-              <a id='msg-link' href={content[0][3]} target="_blank">
+            <div className="col-sm-8.2">
+              <a id="msg-link" href={content[0][3]} target="_blank" rel="noreferrer">
                 {/* <button type="button" className="mybtn Btn btn">
                   Read More!
                 </button> */}
               </a>
             </div>
           </div>
-         {/* 
-<div className="row circles">
-  <div className="col">
-    <i id="P" onClick={customp} className="fa fa-angle-left"></i>
-    <i className="fa fa-circle Indicators" onClick={i1}></i>
-    <i className="fa fa-circle-thin Indicators" onClick={i2}></i>
-    <i id="F" onClick={customf} className="fa fa-angle-right"></i>
-  </div>
-</div>
-*/}
 
-          
+          {/* Uncomment below if you want navigation */}
+          {/*
+          <div className="row circles">
+            <div className="col">
+              <i id="P" onClick={customp} className="fa fa-angle-left"></i>
+              <i className="fa fa-circle Indicators" onClick={i1}></i>
+              <i className="fa fa-circle-thin Indicators" onClick={i2}></i>
+              <i id="F" onClick={customf} className="fa fa-angle-right"></i>
+            </div>
+          </div>
+          */}
         </div>
       </div>
     </div>
