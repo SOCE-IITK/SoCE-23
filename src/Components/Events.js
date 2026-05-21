eb8a1e6aac7e3224410ec10d5280f38256051746
nikhilprajapati-21 <nikhilkumarprajapati2004@gmail.com>
2026-05-21 07:04:55 +0530

    added guest lectures

--- src/Components/Events.js
+++ src/Components/Events.js
@@ -3,11 +3,13 @@ import Events from './Events/Events'
 import Freshers from './Events/Freshers'
 import Colloquium from './Events/Colloquium'
 import Dasant from './Events/Dasant'
+import Profslec from './Events/Profslec'
+
 export default function StudentSec(){
     return(
         <div id='Events'>
-           
             <Events/>
+            <Profslec/>
             <Freshers/>
             <Colloquium/>
             <Dasant/>
